6c98233ada15262d131bee3efa195b30f5ae3e45
bbggkkk <bbggkkk@naver.com>
2022-05-25 00:28:23 +0900

    클라이언트 폴더 추가

---
+++ client/431memo/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import isLogin from './api/isLogin';
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+    </div>
+  )
+}
+
+export default Home
