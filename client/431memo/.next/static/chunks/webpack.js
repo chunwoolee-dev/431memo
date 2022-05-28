30106e6eb80cfb980626b598120efad43b1e332e
bbggkkk <bbggkkk@naver.com>
2022-05-28 17:25:28 +0900

    클라이언트에서 로그인 처리

--- client/431memo/.next/static/chunks/webpack.js
+++ client/431memo/.next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b0f87442aa4a9988"; }
+/******/ 		__webpack_require__.h = function() { return "0164e9ae1895f520"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/431memo/pages/auth.tsx
+++ client/431memo/pages/auth.tsx
@@ -1,31 +1,42 @@
 import axios from 'axios';
 import type { NextPage, NextPageContext } from 'next'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
-const Home: NextPage = () => {
-    return (
-        <></>
-    )
+interface Props {
+  code : string
+}
+
+const Home: NextPage<Props> = (props) => {
+  const router = useRouter();
+  useEffect(() => {
+    const code = props.code;
+    const url = process.env.SERVER;
+    if(code){
+      axios({
+          url : `${url}/callback/google`,
+          method : 'post',
+          data : {
+              code : code
+          }
+      })
+      .then(() => router.push("/"))
+      .catch(() => router.push("/"));
+    }
+  }, []);
+
+  return (
+      <></>
+  )
 }
 
 export async function getServerSideProps(context:NextPageContext) {
-    const code = context.query.code;
-    const url = process.env.SERVER;
-    const res = await axios({
-        url : `${url}/callback/google`,
-        method : 'post',
-        data : {
-            code : code
-        }
-    });
-    return {
-        // props : {
-        //     test : 'good'
-        // }
-      redirect : {
-        destination : '/',
-        permanent : false
-      }
+  const code = context.query.code;
+  return {
+    props : {
+        code : code
     }
   }
+}
 
 export default Home
